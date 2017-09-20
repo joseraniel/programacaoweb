a344911e9847da706dee1358026a0e5da2336324
joseraniel <31082194+joseraniel@users.noreply.github.com>
2017-09-20 15:12:07 -0300

    ProjetoFinal

--- ProjetoAngularJs/js/cep.service.js
+++ ProjetoAngularJs/js/cep.service.js
@@ -1,5 +1,5 @@
-angular.module('app').service('CepService' function($http){
-    this.get = function(cep){
-        return $$http.get('http://api.postmon.com.br/v1/cep/' + cep);
+app.service('CepService', function ($http) {
+    this.get = function (cep) {
+        return $http.get('http://api.postmon.com.br/v1/cep/' + cep);
     };
 });
